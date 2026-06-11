9f8b595bee683884bdd1847a0f97101e46c496c2
dokumentasirektorat1-lgtm <dokumentasirektorat1@gmail.com>
2026-06-11 15:32:21 +0700

    chore: sync local firmware formatting and minor updates

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,24 +1,24 @@
-#include <ESP8266WiFi.h>
-#include <WiFiUDP.h>
-#include "WakeOnLan.h" 
-#include <ESP8266HTTPClient.h>
-#include <WiFiClientSecure.h>
 #include "PubSubClient.h"
+#include "WakeOnLan.h"
 #include "secrets.h"
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiUDP.h>
 
 // --- Configuration ---
-const char* ssid = SECRET_SSID;
-const char* portalUrl = SECRET_PORTAL_URL;
-const char* mqttServer = "broker.emqx.io";
+const char *ssid = SECRET_SSID;
+const char *portalUrl = SECRET_PORTAL_URL;
+const char *mqttServer = "broker.emqx.io";
 const int mqttPort = 1883;
-const char* supabaseUrl = SECRET_SUPABASE_URL;
-const char* supabaseKey = SECRET_SUPABASE_KEY;
+const char *supabaseUrl = SECRET_SUPABASE_URL;
+const char *supabaseKey = SECRET_SUPABASE_KEY;
 
 // Device target (Based on your Agung-PC)
-const char* targetMac = "E8:9C:25:75:7E:C7";
+const char *targetMac = "E8:9C:25:75:7E:C7";
 
 // Topic structure
-String deviceId = "esp01_wol_01"; 
+String deviceId = "esp01_wol_01";
 String cmdTopic = "nyalakanpc/" + deviceId + "/cmd";
 String logTopic = "nyalakanpc/" + deviceId + "/logs";
 String statusTopic = "nyalakanpc/" + deviceId + "/status";
@@ -35,11 +35,11 @@ void sendLog(String msg) {
   }
 }
 
-void triggerWake(const char* mac) {
+void triggerWake(const char *mac) {
   IPAddress ip = WiFi.localIP();
   IPAddress subnet = WiFi.subnetMask();
   IPAddress broadcast = WOL.calculateBroadcastAddress(ip, subnet);
-  
+
   Serial.println("WOL Triggered for: " + String(mac));
   WOL.setBroadcastAddress(broadcast);
   WOL.sendMagicPacket(mac);
@@ -50,14 +50,16 @@ void triggerWake(const char* mac) {
 
 // --- Supabase Cloud Communication ---
 void syncWithSupabase() {
-  if (WiFi.status() != WL_CONNECTED) return;
+  if (WiFi.status() != WL_CONNECTED)
+    return;
 
   WiFiClientSecure secureClient;
   secureClient.setInsecure(); // Simplify for ESP8266
   HTTPClient http;
 
   // 1. Update Last Seen (Heartbeat)
-  String url = String(supabaseUrl) + "/rest/v1/devices?mac_address=eq." + String(targetMac);
+  String url = String(supabaseUrl) + "/rest/v1/devices?mac_address=eq." +
+               String(targetMac);
   http.begin(secureClient, url);
   http.addHeader("apikey", supabaseKey);
   http.addHeader("Authorization", "Bearer " + String(supabaseKey));
@@ -71,7 +73,7 @@ void syncWithSupabase() {
   http.begin(secureClient, url + "&select=wake_request");
   http.addHeader("apikey", supabaseKey);
   http.addHeader("Authorization", "Bearer " + String(supabaseKey));
-  
+
   int getCode = http.GET();
   if (getCode > 0) {
     String payload = http.getString();
@@ -100,22 +102,28 @@ bool checkInternet() {
 void setupWiFi() {
   delay(10);
   WiFi.begin(ssid);
-  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
   Serial.println("\nWiFi connected. IP: " + WiFi.localIP().toString());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length) {
   String message = "";
-  for (int i = 0; i < length; i++) message += (char)payload[i];
-  if (message.startsWith("WAKE|")) triggerWake(message.substring(5).c_str());
+  for (int i = 0; i < length; i++)
+    message += (char)payload[i];
+  if (message.startsWith("WAKE|"))
+    triggerWake(message.substring(5).c_str());
 }
 
 void reconnect() {
   while (!client.connected() && WiFi.status() == WL_CONNECTED) {
     Serial.println("Attempting MQTT connection...");
-    if (client.connect(deviceId.c_str(), statusTopic.c_str(), 1, true, "offline")) {
+    if (client.connect(deviceId.c_str(), statusTopic.c_str(), 1, true,
+                       "offline")) {
       client.subscribe(cmdTopic.c_str());
-      client.publish(statusTopic.c_str(), "online", true); 
+      client.publish(statusTopic.c_str(), "online", true);
     } else {
       delay(5000);
     }
@@ -132,11 +140,15 @@ void setup() {
 }
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) { setupWiFi(); return; }
-  
-  if (!client.connected()) reconnect();
+  if (WiFi.status() != WL_CONNECTED) {
+    setupWiFi();
+    return;
+  }
+
+  if (!client.connected())
+    reconnect();
   client.loop();
-  
+
   static unsigned long lastCloudSync = 0;
   if (millis() - lastCloudSync > 5000) { // Sync every 5 seconds
     syncWithSupabase();
